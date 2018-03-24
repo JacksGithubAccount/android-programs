0199aa27b94162d26086e2d9aca2b1b7ec5aec50
Jack <jckzhao@yahoo.com>
2018-03-24 19:20:49 -0400

    Add files via upload

---
+++ CannonGame/src/zhao/jackie/cannongame/Line.java
@@ -0,0 +1,13 @@
+package zhao.jackie.cannongame;
+
+import android.graphics.Point;
+public class Line {
+	public Point start;
+	public Point end;
+	
+	public Line()
+	{
+		start = new Point(0,0);
+		end = new Point(0,0);
+	}
+}
